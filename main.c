1eae10ff6783ab9e147b2e9748a1b513ef1bd47a
TravJav <travishaycock_2@hotmail.com>
2017-01-15 19:33:27 -0500

    Update main.c
    Added a function that returns a string example "myFile.txt, myFile.jpg instead of user having to type out the exact path which they will end up creating errors or misplacing the file in their system unable to decrypt the file again. Next change will be focusing on a en/decrypt method that will handle large files such as a mp4... which we can speed the process up a bit by getting creative.

--- main.c
+++ main.c
@@ -52,24 +52,41 @@
 
 
 unsigned char key[32]; // key which is not hard-coded for obvious reasons, key is in users head!
-unsigned char vec[16] = "nombUjH*9)r*7K]{,.?";//use as initialization Vector ideally in production this should be secret.
-
+unsigned char vec[16] = "nombUjH*9)r*7K]{";//use as initialization Vector ideally in production this should be secret.
 void encrypt_process(); //define methods for calling
 void decrypt_process();
+ char *get_extension();
+ char *get_dec_type();
 
 
 
 
 
 
-struct class { //template for future implementation of advanced functions
-    char input_file_path[100]; // input file to methods
-    char output_file_path[100]; // input file to methods
+char *get_extension(){ // remember in C you cannot return arrays! in local func
     
-};
+    printf("\n Please Define File Extension Type **Only Use, png, jpeg, mp3,txt");
+    
+    char buffer[25];
+    
+    scanf("%s", &buffer); // protect against overflow
+    if(strlen(buffer)>23){
+         fprintf(stderr, "File Extension Too Large! \n");
+        return 1;
+    } else
+        if(strcmp(buffer,"png")==0){
+          return "-Encrypted.png";
+        }if(strcmp(buffer, "jpg")==0){
+            return "-Encrypted.jpg";
+        }if(strcmp(buffer, "txt")==0){
+            return "-Encrypted.txt";
+        }if(strcmp(buffer, "mp3")==0){
+            return "-Encrypted.mp3";
+        }
 
-
-
+    printf("\n No File Extension Has Been Inputed!");
+    return 1;
+}
 
 /*=========================================================================|
   Encryption Method below, This Will Take The File And Use The Keys And IV  
@@ -80,14 +97,17 @@ struct class { //template for future implementation of advanced functions
  void encrypt_process(){
 
      const char input_file_path[150];
-     const char str2[150] = "Ciph.txt"; //hold the file ending name to let user know it's ciphered
+    // const char str2[150] = "Ciph.txt"; //hold the file ending name to
      
-  
      
-     printf("Enter Input File Path: ");
-     scanf("%s", input_file_path);
+     char *ext = get_extension(); // our function to get user input for file path
 
-   printf("Entered File Name: %s\n", input_file_path);
+     const char *str2 = ext;
+     
+     printf("\n **** Enter Input File Path: "); //first scanf
+     scanf("%s", &input_file_path);
+
+   printf("\n **** File Name Is: %s\n", input_file_path);
      
 
    FILE *Input_File;
@@ -110,36 +130,37 @@ struct class { //template for future implementation of advanced functions
 
    }else
     
-    printf("Input Password Key You Will Use");
+    printf("\n **** Input Password Key You Will Use");
     scanf("%s", &key); // key will be used for encryption process
      
      
-   
-     char *result = malloc(strlen(input_file_path)+strlen(str2)+1);//check for malloc errors in production
+    char *result = malloc(strlen(input_file_path)+strlen(str2)+1);//check for malloc errors in production
      
      if(result == NULL){
      fprintf(stderr, "malloc() failed: insufficient memory!\n");
     
      }else
      
- 
+       
+
+         
     strcpy(result, input_file_path); // put the two strings together so the ending is our desired cipher.txt
      strcat(result, str2);
 
     Output_file = fopen(result, "wb");//File to be written; cipher text
      
-    ERR_load_crypto_strings();   //ERROR check for openssl lib
+    ERR_load_crypto_strings();   //ERROR check for openssl lib all openssl error checkings
     /* Initialise the library */
     ERR_load_crypto_strings();
     OpenSSL_add_all_algorithms();
     OPENSSL_config(NULL);
     
-    fseek(Input_File, 0, SEEK_END);
+    fseek(Input_File, 0, SEEK_END); // read file size then set back to starting point/ otherwise will read 0
     unsigned long len = ftell(Input_File);
     fseek(Input_File, 0, SEEK_SET);
     
     
-    printf("length of the file is : %lu", len);
+    printf("\n **** Length of the file %s is : %lu",input_file_path, len); // print length of file if user cares
     unsigned long outLen1 = 0;
     unsigned long outLen2 = 0;
     unsigned char *indata = malloc(len);
@@ -156,9 +177,7 @@ struct class { //template for future implementation of advanced functions
     //initiating cipher
     EVP_CIPHER_CTX ctx;
     
-    //EVP_EncryptInit(&ctx,EVP,p_KEY,p_IV);
-    
-    //EVP_EncryptInit(&ctx,EVP_aes_128_cbc(),key,vec);
+
      
      EVP_EncryptInit(&ctx,EVP_aes_256_gcm(),key,vec);
      EVP_EncryptUpdate(&ctx,outdata,&outLen1,indata,len);
@@ -170,15 +189,16 @@ struct class { //template for future implementation of advanced functions
     fclose(Input_File); // free all pointers and clean up
     fclose(Output_file);
      free(result);
+     //free(str2);
     Input_File = NULL;
 
    
-    printf("\n Encryption Process Complete");
+    printf("\n ******Encryption Process Complete");
      printf("\n ***** Do You Have More Requests? y/n*******");
      char choose[1];
      scanf("%c",&choose);
      if(strcmp(choose, "y")== 0){
-         printf("\n***** e for encrypt or d for decrypt");
+         printf("\n ***** e for encrypt or d for decrypt");
          char user_choose[1];
          scanf("%s", &choose);
          if(strcmp(user_choose, "e")== 0){
@@ -190,9 +210,33 @@ struct class { //template for future implementation of advanced functions
  }
 
 
- 
 
 
+char *get_dec_type(){
+  
+    printf("\n Please Define File Extension Type **Only Use, png, jpg, mp3,txt");
+    
+    char buffer[25];
+    
+    scanf("%s", &buffer); // protect against overflow
+    if(strlen(buffer)>23){
+        fprintf(stderr, "File Extension Too Large! \n");
+        return 1;
+    } else
+        if(strcmp(buffer,"png")==0){
+            return "-Decrypted.png";
+        }if(strcmp(buffer, "jpg")==0){
+            return "-Decrypted.jpg";
+        }if(strcmp(buffer, "txt")==0){
+            return "-Decrypted.txt";
+        }if(strcmp(buffer, "mp3")==0){
+            return "-Decrypted.mp3";
+        }
+    
+    printf("\n No File Extension Has Been Inputed!");
+    return 1;
+
+}
 
 /*=========================================================================|
  Decryption Method below, This Will Take The File And Use The Keys And IV
@@ -204,17 +248,22 @@ void decrypt_process(){
  
     
     const char input_file_path[150];
-    const char str2[150] = "-Decrypted.txt"; //hold the file ending name to let user know it's ciphered
+    //const char str2[150] = "-Decrypted.txt"; //hold the file ending name to let user know it's ciphered
+    
+    char *ext = get_dec_type(); // our function to get user input for file path
+    const char *str2 = ext;
+    
+    
+    //***** did this
+   // char file_extension[150];
+   // scanf("%s", file_extension);
     
     
     
     printf("Enter Input File Path Where Your Encrypted File Path Resides: ");
     scanf("%s", input_file_path);
     
-    printf(" \n Entered File Name: %s", input_file_path);
-    
-    
-    
+    printf(" \n *** Entered File Name: %s", input_file_path);
     
     FILE *Input_File;
     FILE *Output_file;
@@ -227,15 +276,13 @@ void decrypt_process(){
    Input_File =fopen(input_file_path, "rb");
     
     
-    //if (!(Input_File = fopen(input_file_path, "r"))){ // Check to see if File exist
+    if (!(Input_File = fopen(input_file_path, "r"))){ // Check to see if File exist
         //perror("errno");
         
-    //} else
+    } else
         
      printf("\n ******Input Password Key You Used*******");
          scanf("%s", &key);
-    
-    
     
     
     char *result = malloc(strlen(input_file_path)+strlen(str2)+1);//check for malloc errors in production
@@ -244,8 +291,20 @@ void decrypt_process(){
         fprintf(stderr, "malloc() failed: insufficient memory!\n");
         
     }else
+        /*----------------------------------------
+         Section Will Prompt For File Extension
+         ----------------------------------------*/
+       printf(" /n ** Which File Extension Is Your File? Options: txt, jpeg, png, mp3"); // use proper file extension
+ 
+    
+    
+    
+    
+   // printf(" \n this is what it says %s", file_extension);
+    
+    
         
-        
+        // Normal .txt
         strcpy(result, input_file_path); // put the two strings together so the ending is our desired cipher.txt
         strcat(result, str2);
     
@@ -266,7 +325,7 @@ void decrypt_process(){
     unsigned long len = ftell(Input_File); // use long for files to be safe cause int is =<2GB
     fseek(Input_File, 0, SEEK_SET); // sent back to original so it doesnt read 0
     
-    printf("\n *******length of the file is : %lu*****", len); //xcode underlines this?
+    printf("\n ******* Length of the file %s is : %lu ",input_file_path, len); //xcode underlines this?
     
     unsigned long outLen1 = 0;
     unsigned long outLen2 = 0;
@@ -295,12 +354,12 @@ void decrypt_process(){
     Input_File = NULL; //extra care
 
     // openssl error callings
-    printf("\n *****Decryption Process Complete");
-    printf("\n *****Do You Have More Requests? y/n");
+    printf("\n ***** Decryption Process Complete");
+    printf("\n ***** Do You Have More Requests? y/n");
     char choose[1];
     scanf("%c",&choose);
     if(strcmp(choose, "y")== 0){
-        printf("\n***** e for encrypt or d for decrypt");
+        printf("\n ***** e for encrypt or d for decrypt");
         char user_choose[1];
         scanf("%s", &choose);
         if(strcmp(user_choose, "e")== 0){
@@ -317,25 +376,28 @@ void decrypt_process(){
 
 // main entry point
 int main(int argc, const char * argv[]) {
-    
+
+  
     char option[5]; //local buffer to get input
-   printf("\n **********Welcome To Encryption And Decryption Services!*********");
+   printf("\n  ******* Welcome To Encryption And Decryption Services!");
+   printf("\n ******* Type e for ecryption or d for decryption (more options later) ");
     
-    printf("\n *******Type e for ecryption or d for decryption (more options later)***** ");
-    
-    scanf("%s", option);
+  scanf("%s", option);
     
     if(strcmp(option, "e")== 0){
-    printf("*********Encryption Process Initiated ");
+    printf(" \n *****Encryption Process Initiated ");
     
      encrypt_process(); //======= encryption method
         
     }else if(strcmp(option,"d")== 0){
-    printf("********Decryption Process Initiated********");
+    printf("\n *****Decryption Process Initiated");
         decrypt_process(); //====== decryption method
     
     }
     
     return 0;
 }
+
+
+
 
